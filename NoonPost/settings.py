78f979e55988526789102fc6b3310d8e24a5a1d6
arshia <arshia.sedghi011@gmail.com>
2025-02-26 02:06:05 +0330

    theme

--- NoonPost/settings.py
+++ NoonPost/settings.py
@@ -127,3 +127,4 @@ media_root=os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL=''
