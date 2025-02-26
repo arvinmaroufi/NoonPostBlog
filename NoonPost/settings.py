a519f9e159e401f167b9f1776ace259c2fd09f54
arshia <arshia.sedghi011@gmail.com>
2025-02-27 02:04:37 +0330

    add core app and update project

--- NoonPost/settings.py
+++ NoonPost/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts'
+    'accounts',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -127,6 +128,6 @@ media_root=os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# LOGIN_REDIRECT_URL='core:home'
-# LOGOUT_REDIRECT_URL='core:home'
+LOGIN_REDIRECT_URL='core:home'
+LOGOUT_REDIRECT_URL='core:home'
 EMAI_BACKEND='django.core.mail.backends.console.EmailBacken'
