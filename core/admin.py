ec9727b4bb35c510916641321023bc2a53cbbe57
soude <soude.amiri17@gmail.com>
2025-03-10 09:35:42 +0330

    add sitesetting

--- core/admin.py
+++ core/admin.py
@@ -10,3 +10,8 @@ class NewsleterAdmin(admin.ModelAdmin):
 @admin.register(models.ContactUs)    
 class ContactUsAdmin(admin.ModelAdmin):
     list_display = ['full_name','email','subject','created']
+    
+    
+@admin.register(models.SiteSetting)
+class SiteSettingAdmin(admin.ModelAdmin):
+    list_display = ['phone','email','address']
--- core/views.py
+++ core/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render , redirect
 from .forms import NewsleterForm,ContactUsForm
+from .models import SiteSetting
 
 # Create your views here.
 
 def home(request):
-    return render(request,"core/home.html",{})
+    site_settings = SiteSetting.objects.first()
+    
+    context = {    
+        'site_settings':site_settings
+    }
+    return render(request,"core/home.html",context)
 
 
 def newsleter(request):
@@ -15,6 +21,7 @@ def newsleter(request):
             return redirect("core:home")
         
 def contactus(request):
+    site_settings = SiteSetting.objects.first()
     if request.method == 'POST':
         form =ContactUsForm(request.POST)
         if form.is_valid():
@@ -22,5 +29,18 @@ def contactus(request):
             return redirect('core:home')
     else:
         form = ContactUsForm()
+        
+    context = {
+        'form':form,
+        'site_settings':site_settings
+    }
+    return render(request,'core/contactus.html',context)
+
+
+def about_us(request):
+    site_settings = SiteSetting.objects.first()
     
-    return render(request,'core/contactus.html',{'form':form})
+    context = {    
+        'site_settings':site_settings
+    }
+    return render(request,"core/aboutus.html",context)
